437f5c1a509a4ed58c25e885f9fb5d38bb93c9d1
Ajay <ajaydkumar11@gmail.com>
2026-06-26 13:02:12 +0530

    hello

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        int n1 = 10; 
+        int n2 = 20;
+        int result = n1 + n2;
+        System.out.println(result);
+
+    }
+}
